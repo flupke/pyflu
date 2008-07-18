3c534af60fe57d10e2b4bd9ca10d6d4b22ea489e
flupke <flupke@532e76a9-45ae-c241-9c6d-8309ac6440cd>
2008-07-18 15:03:33 +0000

    pyflu: renommé subprocess en command
    git-svn-id: http://kawa.selfip.org/svn/projects@493 532e76a9-45ae-c241-9c6d-8309ac6440cd

--- pyflu/command.py
+++ pyflu/command.py
@@ -2,29 +2,31 @@
 Object oriented interface to run commands
 """
 
+
+import subprocess
+
+
 class Command(object):
     """Stores a single GRASS command."""
 
     error_class = Exception
 
-    def __init__(self, cmd, verbose=False, cwd=None, error_class=None):
+    def __init__(self, cmd, cwd=None, error_class=None):
         self.cmd = cmd
-        self.verbose = verbose
         self.cwd = cwd
         self.error_class = error_class or Command.error_class
         
     def run(self, env=None, stop_on_errors=True):
-        if self.verbose:
-            print self.cmd
-            stdout = stderr = None
-        else:
-            stdout = stderr = subprocess.PIPE
+        stdout = stderr = subprocess.PIPE
         process = subprocess.Popen([self.cmd], env=env, cwd=self.cwd, 
                 shell=True, stdout=stdout, stderr=stderr)
         if process.wait():
             if stop_on_errors:
-                raise self.error_class("Command '%s' returned %i" % 
+                raise self.error_class("command '%s' returned %i" % 
                         (self.cmd, process.returncode))
 
     def __repr__(self):
         return "<Command: %s>" % self.cmd
+
+    def __str__(self):
+        return self.cmd
