9b28b176513a1b51ddae71d21901a9fd865829f4
Flupke <luper.rouch@gmail.com>
2009-11-17 15:12:30 +0100

    pyflu: path.sub_path converts paths to absolute paths

--- pyflu/path.py
+++ pyflu/path.py
@@ -1,8 +1,10 @@
 """Path related utilities."""
 
-from os.path import commonprefix
+from os.path import commonprefix, abspath
 
 
 def sub_path(path, parent):
     """Returns the portion of ``path`` that lies under ``parent``"""
+    path = abspath(path)
+    parent = abspath(parent)
     return path[len(commonprefix((path, parent))) + 1:]
